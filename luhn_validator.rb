74a595263d1d97e58a6a0ad9a0d07e624e691914
Hsuan <lala092325@gmail.com>
2023-03-04 02:34:23 +0800

    delete unnecessary code

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -15,14 +15,3 @@ module LuhnValidator
     (10 - (sum % 10)) % 10 == test
   end
 end
-
-# number = "6011368353866440"
-# nums_a = number.to_s.chars.map(&:to_i)
-# test = nums_a.pop
-# test_result = nums_a.reverse.each_with_index.map do |num, index|
-#   (index+1).odd? ? num * 2 : num
-# end
-# puts test_result
-# sum = test_result.reduce(0) { |acc, num| acc + (num > 9 ? num - 9 : num) }
-# puts sum
-# puts 10 - (sum % 10) == test
